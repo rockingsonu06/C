482103bbd08e193058c68ca39e6d25bf6cc58b0b
rockingsonu06 <68211519+rockingsonu06@users.noreply.github.com>
2020-10-01 01:41:54 +0530

    Update functionadd(1).c

--- functionadd(1).c
+++ functionadd(1).c
@@ -12,8 +12,10 @@ void sum()
   int a,b,sum;
   printf("Enter first number:");
   scanf("%d",&a);
+  printf("value of first number=%d");
   printf("Enter second number:");
   scanf("%d",&b);
+  printf("value of second number=%d");
   sum=a+b;
   printf("Sum of two numbers=%d",sum);
 }
